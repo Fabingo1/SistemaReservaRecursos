922fbff34e85a6211c321a5189b1b51993c0b626
Gamboa0516 <stevengamboaledezma@gmail.com>
2026-09-07 23:56:17 -0600

    controller y view de Categoria y Recurso

--- src/test/java/servicios/GestorXMLIT.java
+++ src/test/java/servicios/GestorXMLIT.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class GestorXMLIT {
+class  GestorXMLIT {
 
     @Test
     void guardarYCargarCategorias_devuelveLosMismosDatos(@TempDir Path tempDir) throws IOException {
